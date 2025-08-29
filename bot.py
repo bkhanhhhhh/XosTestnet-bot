ce7e7144c17b1fa50ee85842a8920384684f478f
Oswald <120295393+ox1nec@users.noreply.github.com>
2025-08-29 11:21:14 +0200

    v 1.1.5

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import asyncio
-import os
+import os 
 import subprocess 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -53,6 +53,7 @@ async def main():
                 tasks.append(asyncio.create_task(secret_word(secret_words=secret_words, session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)))
 
         await asyncio.gather(*tasks)
+
 
 
 
